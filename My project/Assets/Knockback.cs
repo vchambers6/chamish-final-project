63022f1442bd73e4586516a8a4e204afe5b6b9c8
JULIA KENNISH <juliakennish11@gmail.com>
2022-05-04 20:51:21 -0400

    2 men and some boxes

---
+++ My project/Assets/Knockback.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    [SerializeField] private float knockbackStrength;
+
+    private void OnCollisionEner(Collision collision) {
+    	Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+
+    	if (rb != null) {
+    		Vector3 direction = collision.transform.position - transform.position;
+    		direction.y = 0;
+
+    		rb.AddForce(direction.normalized * knockbackStrength, ForceMode.Impulse);
+    	}
+    }
+}
