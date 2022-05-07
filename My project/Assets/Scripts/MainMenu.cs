367913db1854b613328cb01b910ada618bb7e598
JULIA KENNISH <juliakennish11@gmail.com>
2022-05-07 16:03:43 -0400

    main menu

---
+++ My project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public string game;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+    	SceneManager.LoadScene(game);
+    }
+
+    public void Quit()
+    {
+    	Application.Quit();
+    	Debug.Log("Quitting");
+    }
+}
